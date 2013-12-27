a1499786384479d216d08cc0c89265990e442456
Mischa S <revmischa@cpan.org>
2013-12-27 10:47:28 -0800

    tabs -> spaces

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+    "database/sql"
+    "fmt"
     "io"
-	_ "github.com/jgallagher/go-libpq"
+    _ "github.com/jgallagher/go-libpq"
     "code.google.com/p/go.net/websocket"
     "net/http"
 )
@@ -19,38 +19,38 @@ var globalClients = make(map[string]*Client)
 
 
 func pglistener(db *sql.DB, messages chan string) {
-	notifications, err := db.Query("LISTEN mychan")
-	if err != nil {
-		fmt.Printf("Could not listen to mychan: %s\n", err)
-		close(messages)
-		return
-	}
-	defer notifications.Close()
+    notifications, err := db.Query("LISTEN mychan")
+    if err != nil {
+        fmt.Printf("Could not listen to mychan: %s\n", err)
+        close(messages)
+        return
+    }
+    defer notifications.Close()
 
-	// tell main() it's okay to spawn the pgnotifier goroutine
+    // tell main() it's okay to spawn the pgnotifier goroutine
 
-	var msg string
-	for notifications.Next() {
-		if err = notifications.Scan(&msg); err != nil {
-			fmt.Printf("Error while scanning: %s\n", err)
-			continue
-		}
-		messages <- msg
-	}
+    var msg string
+    for notifications.Next() {
+        if err = notifications.Scan(&msg); err != nil {
+            fmt.Printf("Error while scanning: %s\n", err)
+            continue
+        }
+        messages <- msg
+    }
 
-	fmt.Printf("Lost database connection ?!")
-	close(messages)
+    fmt.Printf("Lost database connection ?!")
+    close(messages)
 }
 
 func notifier(db *sql.DB) {
-	for i := 0; i < 10; i++ {
-		// WARNING: Postgres does not appear to support parameterized notifications
-		//          like "NOTIFY mychan, $1". Be careful not to expose SQL injection!
-		query := fmt.Sprintf("NOTIFY mychan, 'message-%d'", i)
-		if _, err := db.Exec(query); err != nil {
-			fmt.Printf("error sending NOTIFY: %s\n", err)
-		}
-	}
+    for i := 0; i < 10; i++ {
+        // WARNING: Postgres does not appear to support parameterized notifications
+        //          like "NOTIFY mychan, $1". Be careful not to expose SQL injection!
+        query := fmt.Sprintf("NOTIFY mychan, 'message-%d'", i)
+        if _, err := db.Exec(query); err != nil {
+            fmt.Printf("error sending NOTIFY: %s\n", err)
+        }
+    }
 }
 
 func newClientHandler(ws *websocket.Conn) {
@@ -68,23 +68,23 @@ func startServer(){
 }
 
 func main() {
-	db, err := sql.Open("libpq", "") // assuming localhost, user ok, etc
-	if err != nil {
-		fmt.Printf("could not connect to postgres: %s\n", err)
-		return
-	}
-	defer db.Close()
+    db, err := sql.Open("libpq", "") // assuming localhost, user ok, etc
+    if err != nil {
+        fmt.Printf("could not connect to postgres: %s\n", err)
+        return
+    }
+    defer db.Close()
     go startServer()
-	/*messages := make(chan string)
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go pglistener(db, messages, &wg)
+    /*messages := make(chan string)
+    var wg sync.WaitGroup
+    wg.Add(1)
+    go pglistener(db, messages, &wg)
 
-	// wait until LISTEN was issued, then spawn notifier goroutine
-	wg.Wait()
-	go notifier(db)
+    // wait until LISTEN was issued, then spawn notifier goroutine
+    wg.Wait()
+    go notifier(db)
 
-	for i := 0; i < 10; i++ {
-		fmt.Printf("received notification %s\n", <-messages)
-	}*/
+    for i := 0; i < 10; i++ {
+        fmt.Printf("received notification %s\n", <-messages)
+    }*/
 }
